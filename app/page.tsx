2f72a4b85f913dd12529e6528ac9b57252790a79
tryvailo <tryvailo@gmail.com>
2025-12-22 08:00:38 +0000

    chore: disable splash screen temporarily

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,11 @@ import { useState } from "react"
 import SplashScreen from "@/components/splash-screen"
 import ResultsPage from "@/components/results-page"
 
+// TODO: Set to true to re-enable splash screen
+const SHOW_SPLASH_SCREEN = false
+
 export default function Home() {
-  const [showSplash, setShowSplash] = useState(true)
+  const [showSplash, setShowSplash] = useState(SHOW_SPLASH_SCREEN)
 
   if (showSplash) {
     return <SplashScreen onComplete={() => setShowSplash(false)} />
